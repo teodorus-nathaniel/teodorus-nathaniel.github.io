7699ca5a6dcec92626843932f9f589fae43d6efc
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-01-03 11:52:07 +0700

    added scroll animation

---
+++ js/intersection.js
@@ -0,0 +1,14 @@
+function componentScrolled(entries) {
+	entries.forEach((element) => {
+		if (element.isIntersecting) {
+			element.target.classList.add('is-visible');
+		} else {
+			element.target.classList.remove('is-visible');
+		}
+	});
+}
+
+const observer = new IntersectionObserver(componentScrolled);
+
+const components = document.getElementsByClassName('scroll-to-view');
+Array.from(components).forEach((component) => observer.observe(component));
--- js/skill.js
+++ js/skill.js
@@ -1,39 +1,39 @@
 const data = [
 	{
 		name: 'Javascript / HTML / CSS',
-		rating: 5
+		rating: 5,
 	},
 	{
 		name: 'React JS',
-		rating: 4.5
+		rating: 4.5,
 	},
 	{
 		name: 'Laravel',
-		rating: 4.5
+		rating: 4.5,
 	},
 	{
 		name: 'Python',
-		rating: 4
+		rating: 4,
 	},
 	{
 		name: 'C / C#',
-		rating: 4
+		rating: 4,
 	},
 	{
 		name: 'SQL / MySQL',
-		rating: 3
-	}
+		rating: 3,
+	},
 ];
 
 const skillContainer = document.getElementById('skill-container');
-data.forEach(skill => {
-	let stars = Array.from({ length: skill.rating }).reduce(prev => prev + '<i class="fa fa-star"></i>', '');
+data.forEach((skill) => {
+	let stars = Array.from({ length: skill.rating }).reduce((prev) => prev + '<i class="fa fa-star"></i>', '');
 	if (skill.rating % 1 !== 0) stars += '<i class="fa fa-star-half"></i>';
 
 	skillContainer.insertAdjacentHTML(
 		'beforeend',
 		`                                                       
-        <li>
+        <li class="scroll-to-view">
             <span>${skill.name}</span>
             <div class="rating-bar">
                 ${stars}
