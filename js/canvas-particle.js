067caeac4a2c4e0b55648be9f0ed2195884c0b40
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-01-07 23:53:58 +0700

    added fleeing particle

--- js/canvas-particle.js
+++ js/canvas-particle.js
@@ -1,6 +1,16 @@
 {
+	/*
+	stroke / fill?
+	#233b28 #363b23
+	#014775 #752f01
+	*/
+
+	function getHypotenuse (x, y){
+		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	}
+
 	class Particle {
-		constructor(ctx, mousePos, gravity, maxVelocity, fps, width) {
+		constructor (ctx, mousePos, gravity, maxVelocity, fps, width) {
 			this.ctx = ctx;
 			this.mousePos = mousePos;
 			this.gravity = gravity;
@@ -18,11 +28,12 @@
 			};
 		}
 
-		getGravity() {
+		getGravity () {
 			const diffX = this.mousePos.x - this.position.x;
 			const diffY = this.mousePos.y - this.position.y;
 
-			const hypotenuse = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+			const hypotenuse = getHypotenuse(diffX, diffY);
+
 			const ratio = this.gravity / hypotenuse;
 
 			return {
@@ -31,21 +42,12 @@
 			};
 		}
 
-		updateVelocity(axis, gravity) {
-			if (
-				(this.velocity[axis] > this.maxVelocity && gravity > 0) ||
-				(this.velocity[axis] < -this.maxVelocity && gravity < 0)
-			)
-				return;
-			this.velocity[axis] += gravity / this.fps;
-		}
-
-		updatePosition() {
+		updatePosition () {
 			this.position.x += this.velocity.x;
 			this.position.y += this.velocity.y;
 		}
 
-		update() {
+		update () {
 			const { x, y } = this.getGravity();
 
 			this.updateVelocity('x', x);
@@ -54,13 +56,100 @@
 			this.updatePosition();
 		}
 
-		render() {
+		render () {
 			const { x, y } = this.position;
 
 			this.ctx.beginPath();
-			this.ctx.fillStyle = '#014775';
 			this.ctx.arc(x, y, this.width, 0, 2 * Math.PI);
-			this.ctx.fill();
+			this.ctx.stroke();
+		}
+	}
+
+	class ParticleChasing extends Particle {
+		constructor (ctx, mousePos, gravity, maxVelocity, fps, width) {
+			super(ctx, mousePos, gravity, maxVelocity, fps, width);
+		}
+
+		updateVelocity (axis, gravity) {
+			if (
+				(this.velocity[axis] > this.maxVelocity && gravity > 0) ||
+				(this.velocity[axis] < -this.maxVelocity && gravity < 0)
+			)
+				return;
+			this.velocity[axis] += gravity / this.fps;
+		}
+
+		render () {
+			this.ctx.strokeStyle = '#233b28';
+			super.render();
+		}
+	}
+
+	class ParticleFleeing extends Particle {
+		constructor (ctx, mousePos, gravity, maxVelocity, fps, width, maxRadius) {
+			super(ctx, mousePos, gravity, maxVelocity, fps, width);
+			this.maxRadius = maxRadius;
+			this.isChangingPosition = {
+				x: false,
+				y: false,
+			};
+		}
+
+		updateVelocity (axis, gravity) {
+			const diffX = this.mousePos.x - this.position.x;
+			const diffY = this.mousePos.y - this.position.y;
+
+			const hypotenuse = getHypotenuse(diffX, diffY);
+
+			if (hypotenuse > this.maxRadius) {
+				if (this.isChangingPosition[axis]) {
+					if (
+						(this.position[axis] > this.mousePos[axis] && this.velocity[axis] >= 0) ||
+						(this.position[axis] < this.mousePos[axis] && this.velocity[axis] <= 0)
+					) {
+						this.velocity[axis] = 0;
+						this.isChangingPosition.x = false;
+						this.isChangingPosition.y = false;
+						return;
+					}
+					this.velocity[axis] -= gravity / FPS;
+				} else {
+					if (
+						(this.position[axis] > this.mousePos[axis] && this.velocity[axis] <= 0) ||
+						(this.position[axis] < this.mousePos[axis] && this.velocity[axis] >= 0)
+					) {
+						this.velocity[axis] = 0;
+						return;
+					}
+					this.velocity[axis] += gravity / FPS;
+				}
+			} else {
+				if (Math.abs(this.velocity[axis]) < this.maxVelocity) this.velocity[axis] -= gravity / FPS;
+			}
+		}
+
+		updatePosition () {
+			super.updatePosition();
+			if (this.position.x < 0) {
+				this.position.x = window.innerWidth + this.position.x;
+				this.isChangingPosition.x = true;
+			} else if (this.position.x > window.innerWidth) {
+				this.position.x -= window.innerWidth;
+				this.isChangingPosition.x = true;
+			}
+
+			if (this.position.y < 0) {
+				this.position.y = window.innerHeight + this.position.y;
+				this.isChangingPosition.y = true;
+			} else if (this.position.y > window.innerHeight) {
+				this.position.y -= window.innerHeight;
+				this.isChangingPosition.y = true;
+			}
+		}
+
+		render () {
+			this.ctx.strokeStyle = '#363b23';
+			super.render();
 		}
 	}
 
@@ -71,8 +160,9 @@
 	const ctx = canvas.getContext('2d');
 	const GRAVITY = 2;
 	const MAX_VELOCITY = 4;
+	const MAX_RADIUS = 300;
 	const FPS = 30;
-	const PARTICLES_AMOUNT = 25;
+	const PARTICLES_AMOUNT = 50;
 
 	const mousePos = {
 		x: canvas.width / 2,
@@ -82,13 +172,16 @@
 	let particles;
 	createParticles();
 
-	function createParticles() {
+	function createParticles (){
 		particles = Array.from({ length: PARTICLES_AMOUNT }).map(
-			() => new Particle(ctx, mousePos, GRAVITY, MAX_VELOCITY, FPS, Math.random() * 20)
+			() =>
+				Math.random() > 0.5
+					? new ParticleFleeing(ctx, mousePos, GRAVITY, MAX_VELOCITY, FPS, Math.random() * 20, MAX_RADIUS)
+					: new ParticleChasing(ctx, mousePos, GRAVITY, MAX_VELOCITY, FPS, Math.random() * 20)
 		);
 	}
 
-	function updateMouse({ clientX, clientY }) {
+	function updateMouse ({ clientX, clientY }){
 		mousePos.x = clientX;
 		mousePos.y = clientY - canvas.getBoundingClientRect().top;
 	}
@@ -97,7 +190,7 @@
 
 	let animationReqId;
 
-	function render() {
+	function render (){
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		particles.forEach((particle) => {
 			particle.update();
