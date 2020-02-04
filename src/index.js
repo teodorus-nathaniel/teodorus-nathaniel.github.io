f52ad69e2fc8421c4a6893d6878aa3839f3bd8a0
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-02-04 17:39:27 +0700

    now shapes background works in firefox

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import initIntersection from './modules/intersection.js';
 import initSkills from './modules/skills.js';
 import initSlider from './modules/slider.js';
 
-function initAge (){
-	const age = Math.floor((new Date() - new Date(2000, 11, 9)) / (3600 * 24 * 365 * 1000));
+function initAge() {
+	const age = Math.floor(
+		(new Date() - new Date(2000, 11, 9)) / (3600 * 24 * 365 * 1000)
+	);
 	document.getElementById('age-label').textContent = age + ' years old';
 }
 
-function init (){
+function init() {
 	initAge();
 	initHomeCanvas();
 	initCanvasParticles();
@@ -26,4 +28,4 @@ function init (){
 	initIntersection();
 }
 
-init();
+window.addEventListener('DOMContentLoaded', () => init());
