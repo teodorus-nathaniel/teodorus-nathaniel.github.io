cc95cab936795c11790db1f6ed55fdebff88f55e
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-02-07 21:31:44 +0700

    lazy load img

--- src/functionality-modules/intersection.js
+++ src/functionality-modules/intersection.js
@@ -1,14 +1,29 @@
-function componentScrolled (entries){
-	entries.forEach((element) => {
-		if (element.isIntersecting) {
-			element.target.classList.add('is-visible');
-		}
-	});
-}
-
 const observer = new IntersectionObserver(componentScrolled);
 
 const components = document.getElementsByClassName('scroll-to-view');
+
+function loadImage (image, src){
+	const imageTemp = new Image();
+	imageTemp.src = src;
+	imageTemp.onload = function (){
+		image.src = imageTemp.src;
+		image.classList.remove('placeholder');
+		image.classList.add('loaded');
+	};
+}
+
+function componentScrolled (entries){
+	entries.forEach((element) => {
+		if (element.isIntersecting) {
+			const { src } = element.target.dataset;
+			if (src && element.target.tagName === 'IMG') {
+				loadImage(element.target, src);
+			}
+			element.target.classList.add('is-visible');
+			observer.unobserve(element.target);
+		}
+	});
+}
 
 export default function initIntersection (){
 	Array.from(components).forEach((component) => observer.observe(component));
--- src/functionality-modules/projects.js
+++ src/functionality-modules/projects.js
@@ -13,12 +13,12 @@ const projects = [
 
 const container = document.getElementById('projects-container');
 
-export default function initProjects() {
+export default function initProjects (){
 	projects.forEach(
 		(project) =>
 			(container.innerHTML += `
 				<a href="${project.link}" target="_blank" rel="noreferrer">
-					<div class="project-image" style="background-image: url(${project.imgPath})"></div>
+					<img class="project-image placeholder scroll-to-view" src="./img/loading.gif" data-src="${project.imgPath}" alt="${project.name}">
 					<div class="project-info">
 						<span class="title">${project.name}</span>
 						<span>Click to visit</span>
