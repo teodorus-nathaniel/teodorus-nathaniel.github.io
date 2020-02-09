ff7b5bf081ed3cc7df9185770df366dd5e08df71
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-02-09 20:07:05 +0700

    added noscript

--- src/utility-modules/window-resize-controller.js
+++ src/utility-modules/window-resize-controller.js
@@ -2,14 +2,12 @@ const componentsToRescale = [];
 
 window.addEventListener('resize', () => {
 	componentsToRescale.forEach((component) => {
-		console.log({ h: window.innerHeight, w: window.innerWidth });
 		component.width = document.documentElement.clientWidth;
 		component.height = document.documentElement.clientHeight;
 	});
 });
 
 export default function autoRescaleComponent (component){
-	console.log('object');
 	component.width = window.innerWidth;
 	component.height = window.innerHeight;
 	componentsToRescale.push(component);
