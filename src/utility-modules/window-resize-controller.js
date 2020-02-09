e26f7bb2820b4e23045b9292e530cbc2ccc5cf73
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-02-09 11:14:34 +0700

    window resize fixed

--- src/utility-modules/window-resize-controller.js
+++ src/utility-modules/window-resize-controller.js
@@ -2,12 +2,14 @@ const componentsToRescale = [];
 
 window.addEventListener('resize', () => {
 	componentsToRescale.forEach((component) => {
-		component.width = window.innerWidth;
-		component.height = window.innerHeight;
+		console.log({ h: window.innerHeight, w: window.innerWidth });
+		component.width = document.documentElement.clientWidth;
+		component.height = document.documentElement.clientHeight;
 	});
 });
 
 export default function autoRescaleComponent (component){
+	console.log('object');
 	component.width = window.innerWidth;
 	component.height = window.innerHeight;
 	componentsToRescale.push(component);
