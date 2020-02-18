1881506b12b23f81cf65efef3406a8b16c282a5e
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-02-18 22:24:01 +0700

    sticky section

--- src/functionality-modules/intersection.js
+++ src/functionality-modules/intersection.js
@@ -3,28 +3,29 @@ const observer = new IntersectionObserver(componentScrolled);
 const components = document.getElementsByClassName('scroll-to-view');
 
 function loadImage (image, src){
-	const imageTemp = new Image();
-	imageTemp.src = src;
-	imageTemp.onload = function (){
-		image.src = imageTemp.src;
-		image.classList.remove('placeholder');
-		image.classList.add('loaded');
-	};
+  const imageTemp = new Image();
+  imageTemp.src = src;
+  imageTemp.onload = function (){
+    image.src = imageTemp.src;
+    image.classList.remove('placeholder');
+    image.classList.add('loaded');
+  };
 }
 
 function componentScrolled (entries){
-	entries.forEach((element) => {
-		if (element.isIntersecting) {
-			const { src } = element.target.dataset;
-			if (src && element.target.tagName === 'IMG') {
-				loadImage(element.target, src);
-			}
-			element.target.classList.add('is-visible');
-			observer.unobserve(element.target);
-		}
-	});
+  entries.forEach((element) => {
+    if (element.isIntersecting) {
+      const { src } = element.target.dataset;
+      if (src && element.target.tagName === 'IMG') {
+        loadImage(element.target, src);
+      }
+      element.target.classList.add('is-visible');
+    } else {
+      element.target.classList.remove('is-visible');
+    }
+  });
 }
 
 export default function initIntersection (){
-	Array.from(components).forEach((component) => observer.observe(component));
+  Array.from(components).forEach((component) => observer.observe(component));
 }
