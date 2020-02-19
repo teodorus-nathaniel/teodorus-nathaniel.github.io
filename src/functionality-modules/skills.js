ecfa5d12ca363bfcccb32b4a9ef6051062dd5793
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-02-19 20:48:42 +0700

    no more sticky section

--- src/functionality-modules/skills.js
+++ src/functionality-modules/skills.js
@@ -1,40 +1,43 @@
 const data = [
-	{
-		name: 'Javascript / HTML / CSS',
-		rating: 5,
-	},
-	{
-		name: 'React JS',
-		rating: 4.5,
-	},
-	{
-		name: 'Laravel',
-		rating: 4.5,
-	},
-	{
-		name: 'Python',
-		rating: 4,
-	},
-	{
-		name: 'C / C#',
-		rating: 4,
-	},
-	{
-		name: 'SQL / MySQL',
-		rating: 3,
-	},
+  {
+    name: 'Javascript / HTML / CSS',
+    rating: 5,
+  },
+  {
+    name: 'React JS',
+    rating: 4.5,
+  },
+  {
+    name: 'Node JS',
+    rating: 4.5,
+  },
+  {
+    name: 'Typescript',
+    rating: 4,
+  },
+  {
+    name: 'Python',
+    rating: 4,
+  },
+  {
+    name: 'Laravel',
+    rating: 4,
+  },
 ];
 
 const skillContainer = document.getElementById('skill-container');
 
 export default function initSkills (){
-	data.forEach((skill) => {
-		let stars = Array.from({ length: skill.rating }).reduce((prev) => prev + '<i class="fa fa-star"></i>', '');
-		if (skill.rating % 1 !== 0) stars += '<i class="fa fa-star-half"></i>';
+  data.forEach((skill) => {
+    let stars = Array.from({ length: skill.rating }).reduce(
+      (prev) => prev + '<i class="fa fa-star"></i>',
+      ''
+    );
+    if (skill.rating % 1 !== 0) stars += '<i class="fa fa-star-half"></i>';
 
-		skillContainer.insertAdjacentHTML(
-			'beforeend',
-			`                                                       
+    skillContainer.insertAdjacentHTML(
+      'beforeend',
+      `                                                       
 					<li class="scroll-to-view">
 							<span>${skill.name}</span>
 							<div class="rating-bar">
@@ -42,6 +45,6 @@ export default function initSkills (){
 							</div>
 					</li>
 					`
-		);
-	});
+    );
+  });
 }
--- src/utility-modules/color.js
+++ src/utility-modules/color.js
@@ -1,11 +1,11 @@
 export function getCurrentTextColor (){
-	return getComputedStyle(document.documentElement).getPropertyValue(
-		'--text-color'
-	);
+  return getComputedStyle(document.documentElement).getPropertyValue(
+    '--text-color'
+  );
 }
 
 export function getColor (baseColor, opacity){
-	opacity = Math.round(new Number(opacity * 255)).toString(16);
-	if (opacity.length === 1) opacity = '0' + opacity;
-	return baseColor + opacity;
+  opacity = Math.round(new Number(opacity * 255)).toString(16);
+  if (opacity.length === 1) opacity = '0' + opacity;
+  return baseColor + opacity;
 }
