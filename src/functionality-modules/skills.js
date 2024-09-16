03a5f52489cca3bcda51152ee63d1b3306c5ad32
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-09-16 20:26:39 +0700

    Edit skills

--- src/functionality-modules/skills.js
+++ src/functionality-modules/skills.js
@@ -1,19 +1,27 @@
 const data = [
   {
-    name: 'Javascript / HTML / CSS',
-    rating: 5,
+    name: 'Typescript',
+    rating: 4.5,
   },
   {
     name: 'React JS',
     rating: 4.5,
   },
   {
-    name: 'Typescript',
+    name: 'Next JS',
+    rating: 4.5,
+  },
+  {
+    name: 'GraphQL',
     rating: 4.5,
   },
   {
     name: 'Node JS',
-    rating: 4,
+    rating: 4.5,
+  },
+  {
+    name: 'Svelte',
+    rating: 4.5,
   },
   {
     name: 'Python',
@@ -23,28 +31,19 @@ const data = [
     name: 'Laravel',
     rating: 4,
   },
-];
+]
 
-const skillContainer = document.getElementById('skill-container');
+const skillContainer = document.getElementById('skill-container')
 
 export default function initSkills() {
   data.forEach((skill) => {
-    let stars = Array.from({ length: skill.rating }).reduce(
-      (prev) => prev + '<i class="fa fa-star"></i>',
-      ''
-    );
-    if (skill.rating % 1 !== 0) stars += '<i class="fa fa-star-half"></i>';
-
     skillContainer.insertAdjacentHTML(
       'beforeend',
       `                                                       
 					<li class="scroll-to-view">
 							<span>${skill.name}</span>
-							<div class="rating-bar">
-									${stars}
-							</div>
 					</li>
 					`
-    );
-  });
+    )
+  })
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import initCanvasParticles from './functionality-modules/canvas-particle.js';
-import initShapes from './functionality-modules/shapes-generator.js';
-import initProjects from './functionality-modules/projects.js';
-import initNav from './functionality-modules/nav.js';
-import initHomeCanvas from './functionality-modules/canvas.js';
-import initColorSwitch from './functionality-modules/color-mode.js';
-import initIntersection from './functionality-modules/intersection.js';
-import initSkills from './functionality-modules/skills.js';
-import initSlider from './functionality-modules/slider.js';
+import initCanvasParticles from './functionality-modules/canvas-particle.js'
+import initShapes from './functionality-modules/shapes-generator.js'
+import initProjects from './functionality-modules/projects.js'
+import initNav from './functionality-modules/nav.js'
+import initHomeCanvas from './functionality-modules/canvas.js'
+import initColorSwitch from './functionality-modules/color-mode.js'
+import initIntersection from './functionality-modules/intersection.js'
+import initSkills from './functionality-modules/skills.js'
+import initSlider from './functionality-modules/slider.js'
 
-function initAge (){
-	const age = Math.floor(
-		(new Date() - new Date(2000, 11, 9)) / (3600 * 24 * 365 * 1000)
-	);
-	document.getElementById('age-label').textContent = age + ' years old';
+function initAge() {
+  const age = Math.floor(
+    (new Date() - new Date(2000, 11, 9)) / (3600 * 24 * 365 * 1000)
+  )
+  document.getElementById('age-label').textContent = age + ' years old'
 }
 
-function init (){
-	initAge();
-	initHomeCanvas();
-	initCanvasParticles();
-	initShapes();
-	initNav();
-	initSkills();
-	initProjects();
-	initColorSwitch();
-	initSlider();
-	initIntersection();
+function init() {
+  initAge()
+  initHomeCanvas()
+  initCanvasParticles()
+  initShapes()
+  initNav()
+  initSkills()
+  initProjects()
+  initColorSwitch()
+  initSlider()
+  initIntersection()
 }
 
-window.addEventListener('DOMContentLoaded', () => init());
+window.addEventListener('DOMContentLoaded', () => init())
